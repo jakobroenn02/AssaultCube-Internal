763b1992b6e5472315da8d24111da37e41a45fe7
Jakob <jakob.h.roenn@gmail.com>
2025-10-26 21:59:22 +0100

    Remove trainer documentation and minimal DLL files
    Deleted README.md, STATUS.md, minimal.cpp, and minimal_test.cpp from the trainer directory. This cleans up documentation and test files related to the Assault Cube trainer and its minimal DLL example.

--- trainer/minimal.cpp
+++
@@ -1,8 +0,0 @@
-#include <Windows.h>
-
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved) {
-    if (dwReason == DLL_PROCESS_ATTACH) {
-        MessageBoxA(NULL, "Minimal DLL Loaded!", "Test", MB_OK);
-    }
-    return TRUE;
-}
